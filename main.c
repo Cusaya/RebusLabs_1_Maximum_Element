955a5ff3f20596ff0e8320b5274f259a6b19621d
Cusaya <43957324+Cusaya@users.noreply.github.com>
2018-11-21 15:42:46 +0300

    Final (Calisiyor)

---
+++ main.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+
+int find_max(const int * pointer, const int counter) {
+
+
+	int max = *pointer;
+
+	for (int i = 1; i < counter; i++) //sifir indisli oge'yi zaten max'a atadık.
+		if (*(pointer + i ) > max)
+			max = *(pointer + i);
+
+	return max;
+}
+
+
+int main() {
+
+	int n = 0;
+	char input_line[100];
+
+	int scanned = 0;
+	char type_str[20];
+	int type_int = 0;
+	char value_str[20];
+	int value_int = 0;
+
+	int ones = 0;
+	int twos = 0;
+
+	printf("N giriniz:\n");
+	scanf("%d", &n); 
+	getchar();     //scanf buffer'ında kalan newline'ı almak için
+
+
+	int* sequence = (int *)malloc((n * sizeof(int)));
+	if (sequence == NULL) {
+		printf("No space allocated.");
+	}
+	int* ptr = sequence;
+
+
+	for (int i = 0; i < n; i++) {
+  
+		fgets(input_line, 100, stdin);
+		scanned = sscanf(input_line, "%s %s", type_str, value_str);
+
+		type_int = atoi(type_str);
+		if (scanned == 2) {
+			value_int = atoi(value_str);
+		}
+
+		switch (type_int){
+		case 1:
+			*ptr = value_int;
+			ptr = ptr + 1;
+			++ones;
+			break;
+		case 2:
+			ptr = ptr - 1;
+			++twos;
+			break;
+		case 3:
+			printf("%d\n", find_max(sequence,ones-twos));
+			break;
+		}
+
+  }//for'un
+
+}
